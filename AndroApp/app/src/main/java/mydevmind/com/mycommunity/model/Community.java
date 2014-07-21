ddb2ce0f2bf544be1d700b7fd401399cca8bfe2a
Mugiwaranojo <joan.g.francois@gmail.com>
2014-07-21 17:12:10 +0200

    Class models: Community et Player

--- AndroApp/app/src/main/java/mydevmind/com/mycommunity/model/Community.java
+++ AndroApp/app/src/main/java/mydevmind/com/mycommunity/model/Community.java
@@ -1,7 +1,75 @@
 package mydevmind.com.mycommunity.model;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Joan on 21/07/2014.
  */
 public class Community {
+
+    private String objectId;
+    private String name;
+    private String password;
+    private Date createdAt;
+    private Date updatedAt;
+
+    private Set<Player> players= new HashSet<Player>();
+
+    public String getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(String objectId) {
+        this.objectId = objectId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Set<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Set<Player> players) {
+        this.players = players;
+    }
+
+    public void addPlayer(Player player){
+        this.players.add(player);
+    }
+
+    public void remove(Player player){
+        this.players.remove(player);
+    }
 }
--- AndroApp/app/src/main/java/mydevmind/com/mycommunity/model/Player.java
+++ AndroApp/app/src/main/java/mydevmind/com/mycommunity/model/Player.java
@@ -1,6 +1,8 @@
 package mydevmind.com.mycommunity.model;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Joan on 21/07/2014.
@@ -12,6 +14,8 @@ public class Player {
     private String password;
     private Date createdAt;
     private Date updatedAt;
+
+    private Set<Community> communities= new HashSet<Community>();
 
     public String getObjectId() {
         return objectId;
@@ -53,4 +57,19 @@ public class Player {
         this.updatedAt = updatedAt;
     }
 
+    public Set<Community> getCommunities() {
+        return communities;
+    }
+
+    public void setCommunities(Set<Community> communities) {
+        this.communities = communities;
+    }
+
+    public void addCommunity(Community community){
+        this.communities.add(community);
+    }
+
+    public void removeCommunity(Community community){
+        this.communities.remove(community);
+    }
 }
