c339b3211e7d0c6107ab7c282472e188cf7ad2c0
Mugiwaranojo <joan.g.francois@gmail.com>
2014-07-21 11:14:21 +0200

    Ajout de la page login et de parse.com en fragments

--- AndroApp/app/src/main/java/mydevmind/com/mycommunity/LoginActivity.java
+++ AndroApp/app/src/main/java/mydevmind/com/mycommunity/LoginActivity.java
@@ -16,50 +16,34 @@ import com.parse.ParseQuery;
 
 import java.util.List;
 
+import mydevmind.com.mycommunity.fragment.LoginFragment;
+import mydevmind.com.mycommunity.model.IFragmentActionListener;
+
 /**
  * Created by Joan on 11/07/2014.
  */
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements IFragmentActionListener {
 
-    private final String APP_ID="4UNxW53O9e42UjNxLaGma5foAtZQpE22H2IwZ9y3";
-    private final String CLIENT_KEY="zqk5C0BKHuWmSaIrSfuWFVyH4MRlAd7g3iY9uUCg";
 
-    private EditText loginField, passwordField;
-    private Button loginButton;
+    public static final Integer LOGIN_ACTION_INSCRIPTION= 1705;
+    private LoginFragment login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
-        setContentView(R.layout.fragment_login);
-        loginField= (EditText) findViewById(R.id.editTextLogin);
-        passwordField= (EditText) findViewById(R.id.editTextPassword);
-        loginButton= (Button) findViewById(R.id.buttonLogin);
+        login = new LoginFragment();
+        login.setActionListener(this);
+        getFragmentManager().beginTransaction()
+                .add(R.id.container, login)
+                .commit();
+    }
 
-        Parse.initialize(this, APP_ID, CLIENT_KEY);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ParseQuery<ParseObject> query = ParseQuery.getQuery("Player");
-                query.whereEqualTo("name", loginField.getText().toString());
-                query.whereEqualTo("password", passwordField.getText().toString());
-
-                Log.d("connection to parse.com :", "name :"+loginField.getText().toString()+
-                                                   " password :"+passwordField.getText().toString());
-
-                query.findInBackground(new FindCallback<ParseObject>() {
-                    @Override
-                    public void done(List<ParseObject> userList, ParseException e) {
-                        if(e==null){
-                            Log.d("login", "Retrieved  "+ userList.size()+ " user");
-                            Toast.makeText(getApplicationContext(), "Connection OK!!!", Toast.LENGTH_SHORT).show();
-                        }else{
-                           Log.d("login", "Error: "+e.getMessage());
-                        }
-                    }
-                });
-            }
-        });
-
+    @Override
+    public void onFragmentAction(Integer action) {
+        if(action.equals(LOGIN_ACTION_INSCRIPTION)){
+            Toast.makeText(getApplicationContext(), "inscription", Toast.LENGTH_SHORT).show();
+        }
     }
 }
